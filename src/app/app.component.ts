7462be1708544a056169d9d7fc2d94f264c5becc
Santosh Yadav <santosh.yadav198613@gmail.com>
2017-11-19 22:07:15 +0530

    hide nav bars when user not logged-in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { EmployeeService } from './service/employee/employee.service';
 
+import { LoginService } from './service/login/login.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,7 +14,8 @@ export class AppComponent {
   title = 'app';
   toggle: boolean = false;
 
-  constructor(private employeeService: EmployeeService) { }
+  constructor(private employeeService: EmployeeService,
+    public loginService: LoginService) { }
 
   addEmployee() {
     this.employeeService.addEmployee();
